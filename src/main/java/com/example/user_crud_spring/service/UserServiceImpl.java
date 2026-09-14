81157df63832d769b6613e1ca9d669b3b36ab14b
Oigul <aj3913@yandex.ru>
2026-09-14 16:51:45 +0300

    Add @Transactional to UserServiceImpl

--- src/main/java/com/example/user_crud_spring/service/UserServiceImpl.java
+++ src/main/java/com/example/user_crud_spring/service/UserServiceImpl.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tools.jackson.databind.ObjectMapper;
 
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
